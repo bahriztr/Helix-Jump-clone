6125819783b7e26816eecbdfad1316c971e16e3f
can çağlar kırıcı <59360242+cancaglar@users.noreply.github.com>
2022-03-17 14:17:38 +0300

    models added

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float JumpEnd = 5f;
+    [SerializeField] private float JumpDuration = .25f;
     private Rigidbody rb;
+
 
     private void Awake() 
     {
@@ -13,12 +17,13 @@ public class BallController : MonoBehaviour
     }
     void Start()
     {
-        
+        //rb.DOJump(new Vector3(0f,JumpEnd,0f), jumpForce, 1, JumpDuration)
     }
 
     public void Jump()
     {
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
     }
 
     private void OnCollisionEnter(Collision other) 
